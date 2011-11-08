73d276812b534cb28b6ca1fdc5f78957a0a80e99
Scott Duncombe <srduncombe@gmail.com>
2011-11-08 11:23:54 -0800

    Set up knockouts fieldModel

---
+++ app/assets/javascripts/models/fieldModel.js
@@ -0,0 +1,16 @@
+function fieldModel(data) {
+
+	if( data.Field || data.field ) { var field = data.field || data.Field; }
+	else { var field = data; }
+
+	// Setting up key properties
+	if( typeof field.list == 'undefined' ||typeof field.id == 'undefined' ||typeof field.name == 'undefined' ) { throw new Error("mising some vital field data"); }
+	this.list = field.list;
+	this.name = field.name;
+	this.id = field.id;
+
+	this.field_type = field.field_type || 'text';
+	this.field_options = field.field_options || '';
+
+	return this;
+}
---
+++ spec/javascripts/models/fieldModel_spec.js
@@ -0,0 +1,20 @@
+describe("fieldModel", function() {
+  it("can create new fields", function() {
+	field = new fieldModel({ list: 'Test', field_type: 'text', id: 9, name: 'name' });
+    expect(field).toBeDefined();
+  });
+	it("throws an error when forgetting list", function() {
+	  expect(function() { var field = new fieldModel( { id: 9, name: 'name' } ); }).toThrow();
+	});
+	it("throws an error when forgetting name", function() {
+	  expect(function() { var field = new fieldModel( { id: 9, list: 'name' } ); }).toThrow();
+	});
+	it("throws an error when forgetting id", function() {
+	  expect(function() { var field = new fieldModel( { list: 'text', id: 9 } ); }).toThrow();
+	});
+	it("sets the correct name", function() {
+		var field =  new fieldModel( { list: 'Test', id: 9, name: 'Named_it' } );
+	  	expect(field.name).toEqual('Named_it');
+	});
+
+});
