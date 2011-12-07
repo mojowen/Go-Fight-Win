e83a95759a5c3b5ffb736e45b57a40bfc3ad1c1d
Scott Duncombe <srduncombe@gmail.com>
2011-12-07 00:38:53 -0800

    Initalizers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfw::Application.config.secret_token = 'c9aff1b550d3164925f2054f1fd073395959f0e2218eaeff04697d0a94fe9fbd62c29c593792e6c3e053d48a622ed00d40a8ec2fc815e06f05f573adf5a91c8a'
