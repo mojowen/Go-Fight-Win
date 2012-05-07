8616af54a9ec73e5311e726bc84477b2f1df6940
Scott Duncombe <srduncombe@gmail.com>
2012-05-07 11:13:08 -0700

    Fixing some scrolling errors

--- app/assets/javascripts/bindings/scrolling.js
+++ app/assets/javascripts/bindings/scrolling.js
@@ -1,24 +1,30 @@
 appDataModel.scrolling = function(argument) {
 	function checkScroll(pos) {
-		var pos =  pos > 0 ? pos : 0
-		if( dataModel.current.view().now() == pos ) return false // If position hasn't changed, end
-		dataModel.current.view().now(pos);
-		var view = ko.toJS(dataModel.current.view),
-			scrollV = dataModel.current.height.scroll()
-		var bottom = pos + 10,
+		var pos =  pos > 0 ? pos : 0,
+			view = ko.toJS(dataModel.current.view)
+
+
+		if( view.now == pos ) return false // If position hasn't changed, end
+
+		var scrollV = dataModel.current.height.scroll(),
+			bottom = pos + 10,
 			top = pos - 10 ;
+
 
 		if( bottom > view.end - 40 ) {
 			var add = bottom - view.end < 0 ? 1 : bottom - view.end;
 			var end = view.end + add < viewModel.filteredRows().length ?  view.end + add : viewModel.filteredRows().length;
 			dataModel.current.view().end( end )
 		}
-		if( top < view.start + 10 ) {
+
+		if( top < view.start + 10 && top > 0 ) {
 			var move = top - view.start > 0 ? -1 : top - view.start;
 			var start = view.start + move > 0 ? view.start + move  : 0;
 			dataModel.current.view().start( start );
 			dataModel.current.view().end( view.end - move )
 		}
+
+		dataModel.current.view().now(pos);
 	}
 
 	appDataModel.rowScroll = function() {
@@ -39,12 +45,12 @@ appDataModel.scrolling = function(argument) {
 
 	// Scrolling
 	$('#scrolling').scroll(function(e) {
-		var d = new Date()
 		$('.hovered').removeClass('hovered');
 		var $this = $('#scrolling')
 		var pos = Math.round( ( $this.scrollTop() + $this.height() ) / 33 - dataModel.current.height.scroll() + 2); // position = scroll of top scroll + possible height of top scroll ~ converted to row number - rows that can fit + 2 or so off the top
 		checkScroll(pos);
-	});
+	}).scrollTop(0)
+
 	$('#scrolling, .scroll_head').scrollsync({targetSelector: '#scrolling', axis : 'x'});
 	$(window).resize(function(e){
 		dataModel.current.height( $(window).height() - 200 )
