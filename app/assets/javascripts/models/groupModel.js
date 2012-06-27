577cf9251a12134695646aa171b1e4d42cf4ba53
Scott Duncombe <srduncombe@gmail.com>
2012-06-27 14:53:11 -0700

    Defaults to 'year' for date options

--- app/assets/javascripts/models/groupModel.js
+++ app/assets/javascripts/models/groupModel.js
@@ -15,12 +15,20 @@ function groupModel(data) {
 		options = ['day','week','month','year'];
 	}
 
+
+
 	this.field = ko.observable(field);
 	this.name = _field.name;
 	this.field_type = _field.field_type;
+
+	if( data.field == 'created_at' ||  data.field == 'updated_at' ) {
+		this.field_type = 'date'
+		options = ['day','week','month','year'];
+	} 
+
 	if( options.length > 0 ) {
 		var pos = options.indexOf(option);
-		option = pos !== -1 ? options[pos] : '';
+		option = pos !== -1 ? options[pos] : options[options.length-1];
 	}
 	this.option = ko.observable(option || data.option);
 	this.options = options;
