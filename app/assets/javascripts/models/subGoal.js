513367032c5813da9d06e0e553e4181debc4d610
Scott Duncombe <srduncombe@gmail.com>
2012-06-27 16:01:33 -0700

    Can load subGoal from data

--- app/assets/javascripts/models/subGoal.js
+++ app/assets/javascripts/models/subGoal.js
@@ -1,7 +1,8 @@
 function subGoal(data) {
-	this.enteredValue = ko.observable(0)
-	this.enteredDate = ko.observable('')
-	this.name = ko.observable('')
+	var data = data || {}
+	this.enteredValue = ko.observable(data.enteredValue || 0)
+	this.enteredDate = ko.observable( data.enteredDate || '')
+	this.name = ko.observable( data.name ||  '')
 	this.date = ko.computed( function() { 
 		var date = this.enteredDate()
 		if( date.length == 4 ) date = 'Jan 1 '+date
