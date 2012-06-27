f3aa0d71ced765cd1b486c6573eb3934eab41940
Scott Duncombe <srduncombe@gmail.com>
2012-06-27 14:54:20 -0700

    Fixing bug where .menu .remove would trigger outside of table

--- app/assets/javascripts/templates/menu.js
+++ app/assets/javascripts/templates/menu.js
@@ -5,7 +5,7 @@ $(document).on({
 		if( row.key() != 'new' ) rows.destroy(row)
 		else rows.remove(row); 
 	} 
-},'.menu .remove')
+},'.gfTable .menu .remove')
 tableModel.__addTemplate('menu','<div class="field_controller" data-bind="template: {name: $parent[$data.{data_field}]() } "></div>',90)
 tableModel.__addTemplate('rowMenu','<span class="remove clickable">Delete</span>')
 tableModel.__addTemplate('newRowMenu','<span class="add clickable">Add</span><span class="remove clickable">Discard</span>')
