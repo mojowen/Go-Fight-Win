8d6ac530f61ddd9deb0098fe0b5952ef5f2e553e
Scott Duncombe <srduncombe@gmail.com>
2012-07-02 09:20:54 -0700

    Fixed bug when mapping a single point

--- app/assets/javascripts/lib/map.js
+++ app/assets/javascripts/lib/map.js
@@ -105,6 +105,7 @@ function gfMap(options) {
 		for (var i=0; i < this.markers.length; i++) {
 			this.markers[i].point.setMap(null)
 		};
+		this.markers = []
 		for (var i=0; i < rawPoints.length; i++) {
 			var row = rawPoints[i], latlng = row[this.field].latlng
 			if( latlng != '' ) {
@@ -141,21 +142,28 @@ function gfMap(options) {
 		var gMap = this.gMap()
 		if( typeof gMap != 'undefined' ) {
 				var points = this.points(),
-					allLats = this.points().map( function(el) { return parseFloat(el.lat) }),
-					allLngs = this.points().map( function(el) { return parseFloat(el.lng) }),
+					allLats = points.map( function(el) { return parseFloat(el.lat) }),
+					allLngs = points.map( function(el) { return parseFloat(el.lng) }),
+					length = points.length,
 					maxLats = Math.max.apply(Math, allLats),
 					minLats = Math.min.apply(Math, allLats),
 					maxLngs =  Math.max.apply(Math, allLngs),
 					minLngs = Math.min.apply(Math, allLngs),
-					globe = 256,
-					angleLng = maxLngs - minLngs,
+					globe = 256
+
+			if( points.length > 1 ) { 
+				var angleLng = maxLngs - minLngs,
 					angleLng = angleLng < 0 ? angleLng + 360 : angleLng,
 					angleLat = maxLats - minLats
 					angle = angleLng > angleLat ? angleLng : angleLat,
 
 					newCenter = new google.maps.LatLng( ( maxLats + minLats ) / 2, ( maxLngs + minLngs ) / 2 ),
-
 					newZoom = Math.floor(Math.log(960 * 360 / angle / globe) / Math.LN2) - 2
+					
+			} else {
+				newCenter = new google.maps.LatLng( points[0].lat, points[0].lng )
+				newZoom = 14
+			}
 
 			 	gMap.setCenter(newCenter)
 			 	gMap.setZoom(newZoom)
